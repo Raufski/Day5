04c0ca64f57da8c452c0cdabd7ef34407777acfe
Raufski <56489125+Raufski@users.noreply.github.com>
2019-10-28 20:00:15 -0400

    my first commit

---
+++ src/test/java/EntryPoint.java
@@ -0,0 +1,16 @@
+public class EntryPoint {
+    public static void main(String[] args) {
+        Cat myCat = new Cat();
+        myCat.name = "Gav";
+        myCat.voice = "Meyau";
+        System.out.println("");
+        Cat mraufsCat = new Cat();
+        mraufsCat.name = "Leila";
+        mraufsCat.voice = "Muyau";
+        System.out.println("");
+
+
+
+
+    }
+}
